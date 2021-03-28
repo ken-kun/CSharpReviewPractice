3e5ba0f57dd7aa8298375ec4a5065958431bbdc9
KevinLy <kevin.ly96@live.ca>
2021-03-28 17:55:28 -0400

    Update

---
+++ CSharpPractice/CSharpPractice/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Containers
+            //string characterName = "Ben";
+            //int characterAge;
+            //characterAge = 55;
+
+
+            //// Writes line on console
+            //Console.WriteLine("There is a man name " + characterName);
+            //Console.WriteLine("He is " + characterAge + " years old");
+
+            //characterName = "Mike";
+
+            //Console.WriteLine("He liked the name " + characterName);
+            //Console.WriteLine("But didnt like to be " + characterAge);
+
+            ////Data Types
+            //string phrase = "Spy Academy";
+            //char grade = 'C';
+            //int age = 30;
+            //double gpa = 3.3;
+            //float pointAverage = 5.0f;
+            //decimal moneyAmount = 3.45M;
+            //bool isMale = false;
+
+
+            /////C# String methoed
+            //string phrase = "input name" + " is Kevin";
+            //Console.WriteLine(phrase.Substring(6,3));
+
+            ////Working with numbers
+            //Console.WriteLine(40+0*8);
+            //Console.WriteLine(6 + (4 - 2) * 8);
+            //Console.WriteLine(5 + 8.2);
+            //Console.WriteLine(5/2.0);
+
+            //int num = 9;
+            //num++;
+            //Console.WriteLine(num);
+
+            //Console.WriteLine(Math.Pow(3,2));
+            //Console.WriteLine(Math.Sqrt(64));
+            //Console.WriteLine(Math.Max(2, 88));
+            //Console.WriteLine(Math.Min(2, 1));
+            //Console.WriteLine(Math.Round(4.5));
+
+            ////Keep console window up
+            //Console.ReadLine();
+
+            ////Getting User Input
+            //Console.Write("Enter first name: ");
+            //string firstName = Console.ReadLine();
+            //Console.Write("Enter last name: ");
+            //string lastName = Console.ReadLine();
+            //Console.Write("Enter age: ");
+            //string age = Console.ReadLine();
+            //Console.WriteLine("Hello " + firstName + " " + lastName + " at age: " + age);
+
+
+            //Calculator, also convert string into number
+            //int num = Convert.ToInt32( "34");
+            //Console.WriteLine(num + 6);
+
+            //Console.Write("Enter a first number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter a second number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(num1 + num2);
+
+
+            ////Mad Libs Game
+            //string color, pluralNoun, celebrity;
+
+            //Console.Write("Enter a color: ");
+            //color = Console.ReadLine();
+            //Console.Write("Enter a plural noun: ");
+            //pluralNoun = Console.ReadLine();
+            //Console.Write("Enter a celebrity: ");
+            //celebrity = Console.ReadLine();
+
+            //Console.WriteLine("Roses are " + color);
+            //Console.WriteLine(pluralNoun + " are blue");
+            //Console.WriteLine("I love " + celebrity);
+
+            ////Arrays
+            //int[] luckyNumbers = { 2, 5, 56, 78, 98, 7};
+            //string[] friends = new string[3];
+            //friends[0] = "Tony";
+            //friends[1] = "Ruslaan";
+            //friends[3] = "Trinh";
+
+
+            ////Console.WriteLine(luckyNumbers[]);
+            //Console.WriteLine(friends[1]);
+
+            ////Methods - SayHi()
+            //SayHi("Kevin", 25);
+            //SayHi("Tony", 56);
+
+            ////Return statement
+            //int cubeNumber = Cube(3);
+            //Console.WriteLine(cubeNumber);
+
+
+            //If Statements
+
+
+
+            //Keep console window up
+            Console.ReadLine();
+        }
+
+        //static void SayHi(string name, int age)
+        //{
+        //    Console.WriteLine("Hello " + name + " you are " + age );
+        //}
+
+        //static int Cube(int num)
+        //{
+        //    int result = num * num * num;
+        //    return result;
+        //}
+
+    }
+}
